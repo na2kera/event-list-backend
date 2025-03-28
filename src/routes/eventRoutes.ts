c5cd797c042ba8ddb27f744debff710a27f64a4b
na2kera <nat3mitaro@gmail.com>
2025-03-28 20:08:42 +0900

    イベント検索のAPI

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -3,11 +3,13 @@ import {
   getEvents,
   getEventById,
   createEvent,
+  searchEvents
 } from "../controllers/eventController";
 
 const router = Router();
 
 router.get("/", getEvents);
+router.get("/search", searchEvents);
 router.get("/:id", getEventById);
 router.post("/", createEvent);
 
